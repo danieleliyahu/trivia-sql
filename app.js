61fea4db7d8fa676ce03bce785aa30bd06b5c2a5
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-04-22 22:39:35 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const Sequelize = require("sequelize");
 
 morgan.token("reqbody", (req) => {
-	const newObject = {};
-	for (const key in req.body) {
-		if (JSON.stringify(req.body[key]).length > 100) {
-			newObject[key] = "Too many to print...";
-			continue;
-		}
-		newObject[key] = req.body[key];
-	}
-	return JSON.stringify(newObject);
+  const newObject = {};
+  for (const key in req.body) {
+    if (JSON.stringify(req.body[key]).length > 100) {
+      newObject[key] = "Too many to print...";
+      continue;
+    }
+    newObject[key] = req.body[key];
+  }
+  return JSON.stringify(newObject);
 });
 
 app.use(express.json());
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :reqbody"));
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :reqbody"
+  )
+);
 app.use(express.static("./client/build"));
 
 const {
-	Country,
-	CrimeIndex,
-	Capital,
-	CostOfLivingIndex,
-	PopulationDensity,
-	QualityOfLifeIndex,
+  Country,
+  CrimeIndex,
+  Capital,
+  CostOfLivingIndex,
+  PopulationDensity,
+  QualityOfLifeIndex,
+  Player,
+  QuestionTemplate,
 } = require("./models");
+
+app.get("/", (req, res) => {
+  Country.findAll({})
+    .then(async (country) => {
+      res.send(country);
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = app;
