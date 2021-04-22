38ef6cc44015c5ad78a26cebdbe3ed0c2a4629a0
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-04-22 23:31:28 +0300

    create route for trivia question
    type 1 questions with 4 options

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const Sequelize = require("sequelize");
 
-morgan.token("reqbody", (req) => {
-  const newObject = {};
-  for (const key in req.body) {
-    if (JSON.stringify(req.body[key]).length > 100) {
-      newObject[key] = "Too many to print...";
-      continue;
-    }
-    newObject[key] = req.body[key];
-  }
-  return JSON.stringify(newObject);
-});
+// morgan.token("reqbody", (req) => {
+//   const newObject = {};
+//   for (const key in req.body) {
+//     if (JSON.stringify(req.body[key]).length > 100) {
+//       newObject[key] = "Too many to print...";
+//       continue;
+//     }
+//     newObject[key] = req.body[key];
+//   }
+//   return JSON.stringify(newObject);
+// });
 
-app.use(express.json());
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :reqbody"
-  )
-);
+// app.use(express.json());
+// app.use(
+//   morgan(
+//     ":method :url :status :res[content-length] - :response-time ms :reqbody"
+//   )
+// );
 app.use(express.static("./client/build"));
 
 const {
@@ -40,6 +40,26 @@ app.get("/", (req, res) => {
       res.send(country);
     })
     .catch((err) => console.log(err));
+});
+
+app.get("/question1", async (req, res) => {
+  const qdata = await QuestionTemplate.findOne({
+    order: Sequelize.literal("rand()"),
+    where: { type: 1 },
+    attributes: ["template", "is_first", "table_name", "column_name"],
+  });
+
+  const odata = await Country.findAll({
+    order: Sequelize.literal("rand()"),
+    attributes: ["name"],
+    limit: 4,
+  });
+
+  const triviaQuestion = { question: qdata, options: odata };
+
+  console.log(JSON.stringify(qdata));
+  console.log(JSON.stringify(odata));
+  res.json(triviaQuestion);
 });
 
 module.exports = app;
