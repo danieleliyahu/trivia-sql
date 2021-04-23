204d31b96d7ea358f348224d3c2eddb5d8057798
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-04-23 12:21:00 +0300

    add function to PascalCase

--- app.js
+++ app.js
@@ -43,22 +43,58 @@ app.get("/", (req, res) => {
 });
 
 app.get("/question1", async (req, res) => {
-  const qdata = await QuestionTemplate.findOne({
+  let questionData = await QuestionTemplate.findOne({
     order: Sequelize.literal("rand()"),
-    where: { type: 1 },
-    attributes: ["template", "is_first", "table_name", "column_name"],
+    where: { type: 2 },
+    attributes: ["template", "is_first", "table_name", "column_name", "type"],
   });
 
-  const odata = await Country.findAll({
-    order: Sequelize.literal("rand()"),
-    attributes: ["name"],
-    limit: 4,
-  });
+  let type = questionData.type;
+  let optionsData = "";
 
-  const triviaQuestion = { question: qdata, options: odata };
+  if (type === 1) {
+    optionsData = await Country.findAll({
+      order: Sequelize.literal("rand()"),
+      attributes: ["name"],
+      limit: 4,
+    });
+  } else if (type === 3 || type === 4) {
+    optionsData = [true, false];
+  } else {
+    const snakeToPascal = (string) => {
+      return string
+        .split("/")
+        .map((snake) =>
+          snake
+            .split("_")
+            .map((substr) => substr.charAt(0).toUpperCase() + substr.slice(1))
+            .join("")
+        )
+        .join("/");
+    };
 
-  console.log(JSON.stringify(qdata));
-  console.log(JSON.stringify(odata));
+    const tableName = questionData.table_name;
+    const modelName = snakeToPascal(tableName);
+    // const columnName = snakeToCamel(questionData.column_name);
+
+    console.log(tableName);
+    console.log(modelName);
+    console.log(typeof modelName);
+
+    // console.log(columnName);
+    optionsData = await Country.findAll({
+      order: Sequelize.literal("rand()"),
+      //   attributes: [columnName],
+      limit: 4,
+    });
+  }
+
+  //table_name: "population_density" model:PopulationDensity
+  const triviaQuestion = { question: questionData, options: optionsData };
+  //   console.log(optionsData);
+  //   console.log(JSON.stringify(questionData));
+  //   console.log(JSON.stringify(optionsData));
+  //   res.json(optionsData);
   res.json(triviaQuestion);
 });
 
