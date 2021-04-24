5b4b2445fc951f0cb5376893e54253bbbb3448a3
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-04-24 14:42:58 +0300

    create question component

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,55 @@
 import "./App.css";
 import axios from "axios";
+import Question from "./components/Question";
 import { useEffect, useState } from "react";
 
 function App() {
   const [question, setQuestion] = useState([]);
 
-
-  
-  useEffect(() => {
-  axios.get(`http://localhost:3001/question`).then(({ data }) => {
-    console.log(data.question);
-    
+  const getQuestion = () =>
+    axios
+      .get(`/question`)
+      .then(({ data }) => {
         console.log(data);
-        if(data){
-          const clients = data.map((client, i) => {
-            console.log(client)
-              return (
-                <>
-              <h1 key={i} className={"question"}>
-                {client.question.template} </h1>)
-                <div key={i} className={"options"}>
-                {client.question.template}
+        if (data) {
+          const fullQuestion = data.map((fullQuestion, i) => {
+            console.log(fullQuestion);
+            return (
+              <>
+                <div key={i}>
+                  <h1 className={"question"}>
+                    {fullQuestion.question.template}
+                  </h1>
+                  <div className={"options"}>{fullQuestion.options.answer}</div>
+                  <div className={"options"}>
+                    {fullQuestion.options.option1}
+                  </div>
+                  <div className={"options"}>
+                    {fullQuestion.options.option2}
+                  </div>
+                  <div className={"options"}>
+                    {fullQuestion.options.option3}
+                  </div>
                 </div>
-                <div key={i} className={"options"}>
-                {client.question.template}
-                </div>
-                <div key={i} className={"options"}>
-                {client.question.template}
-                </div>
-                </>
-          )})
-            setQuestion(clients);
+              </>
+            );
+          });
+          setQuestion(fullQuestion);
         }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-    
+  useEffect(() => {
+    getQuestion();
+  }, []);
 
-  })
-    // .catch((error) => { console.log("There was an error", error); })
-},[])
-
-  return <div>
-    <p>{question}</p>
-  </div>;
+  return (
+    <div>
+      <Question question={question} />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Question.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Question = ({ question }) => {
+  return (
+    <div>
+      <p>{question}</p>
+    </div>
+  );
+};
+
+export default Question;
